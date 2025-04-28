c6bf9c6f29c396ff494c0c8d339aabe363f335d5
Alexandru Szabo <alexandru.szabo@icloud.com>
2025-04-28 18:37:32 +0300

    backgroud

--- vivo/lib/main.dart
+++ vivo/lib/main.dart
@@ -85,33 +85,23 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+     body: Container(
+  decoration: BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage("images/background1.png"), // Replace with your actual image path
+      fit: BoxFit.cover, // Ensures the image covers the full screen
+    ),
+  ),
+  child: Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text('Welcome to the Christian Reunion App'),
+        // Add any other widgets you need inside
+      ],
+    ),
+  ),
+),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
